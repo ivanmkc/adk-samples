52ddf9db7610b1f0d2e20cb8d3c53115e795eae3
ivanmkc@google.com <ivanmkc@google.com>
2025-07-11 00:22:11 -0400

    Added revision code to autorater

--- python/agents/llm-auditor/llm_auditor/claims.py
+++ python/agents/llm-auditor/llm_auditor/claims.py
@@ -13,5 +13,5 @@ class Claim:
     is_supported: bool = dataclasses.field(metadata={"description": "Whether the answer is supported by the corpus or not"})
     is_supported_after_rewriting: bool = dataclasses.field(metadata={"description": "After rewriting, whether the answer is supported by the corpus or not. This should ideally match `is_supported`."})
     reasoning: str = dataclasses.field(metadata={"description": "The reasoning for `is_supported_after_rewriting`."})
-    source_file_path: pathlib.Path = dataclasses.field(metadata={"description": "Source file for which the query/answer applies to."})
+    source_file_path: str = dataclasses.field(metadata={"description": "Source file for which the query/answer applies to."})
     ambiguity: QueryAmbiguity
